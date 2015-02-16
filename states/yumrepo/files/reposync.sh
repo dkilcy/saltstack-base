3f6f9c620e25e9e358e5855fc07bf8084ec50cf0
devops <devops@workstation2.pub>
2015-02-16 18:41:45 -0500

    update reposync.sh to point to /var/repo/yum

--- states/yumrepo/files/reposync.sh
+++ states/yumrepo/files/reposync.sh
@@ -3,38 +3,38 @@
 
 ### CentOS 6
 
-mkdir -p /var/yum/repo/centos/6/os
-mkdir -p /var/yum/repo/centos/6/updates
-mkdir -p /var/yum/repo/centos/6/extras
+mkdir -p /var/repo/yum/centos/6/os
+mkdir -p /var/repo/yum/centos/6/updates
+mkdir -p /var/repo/yum/centos/6/extras
 
-cd /var/yum/repo/centos/6
+cd /var/repo/yum/centos/6
 
 rsync -avrt rsync://mirror.umd.edu/centos/6/os .
 rsync -avrt rsync://mirror.umd.edu/centos/6/updates .
 rsync -avrt rsync://mirror.umd.edu/centos/6/extras .
 
-cd /var/yum/repo/centos/6/epel
+cd /var/repo/yum/centos/6/epel
 rsync -avrt rsync://mirror.pnl.gov/epel/6/i386 .
 rsync -avrt rsync://mirror.pnl.gov/epel/6/x86_64 .
 
 
 ### CentOS 7
 
-mkdir -p /var/yum/repo/centos/7/os
-mkdir -p /var/yum/repo/centos/7/updates
-mkdir -p /var/yum/repo/centos/7/extras
-mkdir -p /var/yum/repo/centos/7/epel
-mkdir -p /var/yum/repo/centos/7/openstack-juno
+mkdir -p /var/repo/yum/centos/7/os
+mkdir -p /var/repo/yum/centos/7/updates
+mkdir -p /var/repo/yum/centos/7/extras
+mkdir -p /var/repo/yum/centos/7/epel
+mkdir -p /var/repo/yum/centos/7/openstack-juno
 
-cd /var/yum/repo/centos/7
+cd /var/repo/yum/centos/7
 
 rsync -avrt rsync://mirror.umd.edu/centos/7/os .
 rsync -avrt rsync://mirror.umd.edu/centos/7/updates .
 rsync -avrt rsync://mirror.umd.edu/centos/7/extras .
 
-reposync -p /var/yum/repo/centos/7 --repoid=epel
-reposync -p /var/yum/repo/centos/7 --repoid=openstack-juno
+reposync -p /var/repo/yum/centos/7 --repoid=epel
+reposync -p /var/repo/yum/centos/7 --repoid=openstack-juno
 
-createrepo /var/yum/repo/centos/7/epel
-createrepo /var/yum/repo/centos/7/openstack-juno
+createrepo /var/repo/yum/centos/7/epel
+createrepo /var/repo/yum/centos/7/openstack-juno
 
