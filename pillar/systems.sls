ea3aded6dceadc1ce8fe3aad26519beb0d405653
devops <devops@workstation2.pub>
2015-06-14 21:30:24 -0400

    Add conn-1 and new reposync

--- pillar/systems.sls
+++ pillar/systems.sls
@@ -66,11 +66,57 @@ systems:
       ipaddr: 10.0.0.11
       nic0: enp0s20f0
       nic1: enp0s20f1
-      team_config: '{"runnner":{"name":"balance-xor"}}'
+      team_config: '{"runnner":{"name":"activebackup"}}'
     team1:
       ipaddr: 192.168.1.11
       nic0: enp0s20f2
       nic1: enp0s20f3
-      team_config: '{"runnner":{"name":"balance-xor"}}'
+      team_config: '{"runnner":{"name":"activebackup"}}'
+{% elif grains['id'] == 'network1' %}
+  network:
+    team0:
+      ipaddr: 10.0.0.21
+      nic0: enp0s20f0
+      nic1: enp0s20f1
+      team_config: '{"runnner":{"name":"activebackup"}}'
+    team1:
+      ipaddr: 10.0.1.21
+      nic0: enp0s20f2
+      nic1: enp0s20f3
+      team_config: '{"runnner":{"name":"activebackup"}}'
+{% elif grains['id'] == 'compute1' %}
+  network:
+    team0:
+      ipaddr: 10.0.0.31
+      nic0: enp0s20f0
+      nic1: enp0s20f1
+      team_config: '{"runnner":{"name":"activebackup"}}'
+    team1:
+      ipaddr: 10.0.1.31
+      nic0: enp0s20f2
+      nic1: enp0s20f3
+      team_config: '{"runnner":{"name":"activebackup"}}'
+{% elif grains['id'] == 'compute2' %}
+  network:
+    team0:
+      ipaddr: 10.0.0.32
+      nic0: enp0s20f0
+      nic1: enp0s20f1
+      team_config: '{"runnner":{"name":"activebackup"}}'
+    team1:
+      ipaddr: 10.0.1.32
+      nic0: enp0s20f2
+      nic1: enp0s20f3
+      team_config: '{"runnner":{"name":"activebackup"}}'
+{% elif grains['id'] == 'conn1' %}
+  network:
+    bond0:
+      ipaddr: 10.0.0.51
+      nic0: eth0
+      nic1: eth1
+    bond1:
+      ipaddr: 10.0.1.51
+      nic0: eth2
+      nic1: eth3
 {% endif %}
 
--- states/yumrepo/files/reposync.sh
+++ states/yumrepo/files/reposync.sh
@@ -1,7 +1,9 @@
 
 ##exit
 
+####################################################################################
 ### CentOS 6
+####################################################################################
 
 mkdir -p /var/repo/yum/centos/6/os
 mkdir -p /var/repo/yum/centos/6/updates
@@ -13,18 +15,28 @@ rsync -avrt rsync://mirror.umd.edu/centos/6/os .
 rsync -avrt rsync://mirror.umd.edu/centos/6/updates .
 rsync -avrt rsync://mirror.umd.edu/centos/6/extras .
 
+####################################################################################
+### CentOS 6 - EPEL 
+####################################################################################
+
+mkdir -p /var/repo/yum/centos/6/epel
+
 cd /var/repo/yum/centos/6/epel
+
 rsync -avrt rsync://mirror.pnl.gov/epel/6/i386 .
 rsync -avrt rsync://mirror.pnl.gov/epel/6/x86_64 .
 
+####################################################################################
+### CentOS 6 - Scality Lorien
+####################################################################################
 
+####################################################################################
 ### CentOS 7
+####################################################################################
 
 mkdir -p /var/repo/yum/centos/7/os
 mkdir -p /var/repo/yum/centos/7/updates
 mkdir -p /var/repo/yum/centos/7/extras
-mkdir -p /var/repo/yum/centos/7/epel
-mkdir -p /var/repo/yum/centos/7/openstack-juno
 
 cd /var/repo/yum/centos/7
 
@@ -32,9 +44,33 @@ rsync -avrt rsync://mirror.umd.edu/centos/7/os .
 rsync -avrt rsync://mirror.umd.edu/centos/7/updates .
 rsync -avrt rsync://mirror.umd.edu/centos/7/extras .
 
-reposync -p /var/repo/yum/centos/7 --repoid=epel
-reposync -p /var/repo/yum/centos/7 --repoid=openstack-juno
+####################################################################################
+### CentOS 7 - EPEL
+####################################################################################
 
-createrepo /var/repo/yum/centos/7/epel
-createrepo /var/repo/yum/centos/7/openstack-juno
+mkdir -p /var/repo/yum/centos/7/epel
+
+cd /var/repo/yum/centos/7/epel
+
+rsync -avrt rsync://mirror.pnl.gov/epel/7/x86_64 .
+
+####################################################################################
+
+exit
+
+####################################################################################
+### CentOS 7 - Scality Lorien
+####################################################################################
+
+reposync -p /var/repo/yum/centos/7 --repoid=scality-lorien
+createrepo /var/repo/yum/centos/7/scality-lorien
+
+####################################################################################
+### CentOS 7 - Openstack Kilo
+####################################################################################
+
+reposync -p /var/repo/yum/centos/7 --repoid=openstack-kilo
+createrepo /var/repo/yum/centos/7/openstack-kilo
+
+####################################################################################
 
