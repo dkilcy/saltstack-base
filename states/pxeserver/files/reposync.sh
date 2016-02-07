95bc0648c21fcbf94b7a16bbe77a5a3b15a69213
devops <devops@workstation2.pub>
2016-02-07 15:05:02 -0500

    update

--- states/pxeserver/files/reposync.sh
+++ states/pxeserver/files/reposync.sh
@@ -11,9 +11,9 @@ mkdir -p /var/www/html/repo/centos/6/extras
 
 cd /var/www/html/repo/centos/6
 
-rsync -avrt --exclude 'i386*' --exclude 'debug' rsync://mirror.umd.edu/centos/6/os .
-rsync -avrt --exclude 'i386*' --exclude 'debug' rsync://mirror.umd.edu/centos/6/updates .
-rsync -avrt --exclude 'i386*' --exclude 'debug' rsync://mirror.umd.edu/centos/6/extras .
+rsync -avrt --delete --exclude 'i386*' --exclude 'debug' --exclude 'drpms' rsync://mirror.umd.edu/centos/6/os .
+rsync -avrt --delete --exclude 'i386*' --exclude 'debug' --exclude 'drpms' rsync://mirror.umd.edu/centos/6/updates .
+rsync -avrt --delete --exclude 'i386*' --exclude 'debug' --exclude 'drpms' rsync://mirror.umd.edu/centos/6/extras .
 
 ####################################################################################
 ### CentOS 6 - EPEL 
@@ -24,18 +24,17 @@ mkdir -p /var/www/html/repo/centos/6/epel
 cd /var/www/html/repo/centos/6/epel
 
 #rsync -avrt rsync://mirror.pnl.gov/epel/6/i386 .
-rsync -avrt --exclude 'debug' rsync://mirror.pnl.gov/epel/6/x86_64 .
+rsync -avrt --delete --exclude 'debug' --exclude 'drpms' rsync://mirror.pnl.gov/epel/6/x86_64 .
 
 ####################################################################################
 ### CentOS 6 - Scality Lorien
 ####################################################################################
 
-reposync -p /var/www/html/repo/centos/6/scality-lorien/x86_64 --repoid=scality-lorien-centos6 --norepopath
-createrepo /var/www/html/repo/centos/6/scality-lorien/x86_6
-
 reposync -p /var/www/html/repo/centos/6/scality-lorien1/x86_64 --repoid=scality-lorien1-centos6 --norepopath
 createrepo /var/www/html/repo/centos/6/scality-lorien1/x86_64
 
+reposync -p /var/www/html/repo/centos/6/scality-lorien/x86_64 --repoid=scality-lorien-centos6 --norepopath
+createrepo /var/www/html/repo/centos/6/scality-lorien/x86_64
 
 ####################################################################################
 ### CentOS 7
@@ -47,9 +46,9 @@ mkdir -p /var/www/html/repo/centos/7/extras
 
 cd /var/www/html/repo/centos/7
 
-rsync -avrt --exclude 'debug' rsync://mirror.umd.edu/centos/7/os .
-rsync -avrt --exclude 'debug' rsync://mirror.umd.edu/centos/7/updates .
-rsync -avrt --exclude 'debug' rsync://mirror.umd.edu/centos/7/extras .
+rsync -avrt --delete --exclude 'debug' --exclude 'drpms' rsync://mirror.umd.edu/centos/7/os .
+rsync -avrt --delete --exclude 'debug' --exclude 'drpms' rsync://mirror.umd.edu/centos/7/updates .
+rsync -avrt --delete --exclude 'debug' --exclude 'drpms' rsync://mirror.umd.edu/centos/7/extras .
 
 ####################################################################################
 ### CentOS 7 - EPEL
@@ -57,7 +56,7 @@ rsync -avrt --exclude 'debug' rsync://mirror.umd.edu/centos/7/extras .
 
 mkdir -p /var/www/html/repo/centos/7/epel
 cd /var/www/html/repo/centos/7/epel
-rsync -avrt --exclude 'debug' rsync://mirror.pnl.gov/epel/7/x86_64 .
+rsync -avrt --delete --exclude 'debug'  --exclude 'drpms' rsync://mirror.pnl.gov/epel/7/x86_64 .
 
 ####################################################################################
 
@@ -75,3 +74,22 @@ createrepo /var/www/html/repo/centos/7/openstack-kilo
 reposync -p /var/www/html/repo/centos/7/scality-lorien1/x86_64 --repoid=scality-lorien1-centos7 --norepopath
 createrepo /var/www/html/repo/centos/7/scality-lorien1/x86_64
 
+reposync -p /var/www/html/repo/centos/7/scality-lorien/x86_64 --repoid=scality-lorien-centos7 --norepopath
+createrepo /var/www/html/repo/centos/7/scality-lorien/x86_64
+
+####################################################################################
+
+chown -R root.root /var/www/html/repo/*
+
+####################################################################################
+# 
+# rsync to USB stick:
+#
+# $ mount /dev/sdb1 /mnt
+# $ rsync -avt --delete /var/www/html/repo/ /mnt/repo
+#
+# rsync the to another system:
+#
+# $ rsync -avtz --delete /var/www/html/repo/ workstation1:/var/www/html/repo
+#
+####################################################################################
